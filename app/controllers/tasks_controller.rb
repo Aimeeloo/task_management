d5d4a9ff6b0a3694fb6ca0914fa6775f44f7dfc2
A Min <nuomi29@gmail.com>
2019-11-06 15:50:41 +0800

    CRUD index/new/create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,24 @@
 class TasksController < ApplicationController
   def index
-    
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to root_path, notice: "新增成功"
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :category, :description, :priority, :status, :start_at, :end_at)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ApplicationRecord
-  belongs_to :user
   has_many :taggings
   has_many :tags, through: :taggings
 end
