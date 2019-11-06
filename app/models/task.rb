d60a600cc29a2fc6f8277967242115ca2bcae58c
A Min <nuomi29@gmail.com>
2019-11-06 23:23:54 +0800

    column priority & status use enum in Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
+
+  enum priority: { '普通': 0, '有點趕': 1, '緊急事件': 2 }
+  enum status: { '尚未完成': 0, '進行中': 1, '已結束': 2 }
 end
