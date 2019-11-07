132555b9494d8bf09772a2c5ec449001422ccdf3
A Min <nuomi29@gmail.com>
2019-11-08 00:00:17 +0800

    change enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
-  enum category: [ '居家', '工作', '娛樂', '學習', '其他' ]
-  enum priority: [ '普通', '有點趕', '緊急事件' ]
-  enum status: [ '尚未完成', '進行中', '已結束' ]
+  enum category: [ :home, :work, :entertainment, :learn, :other ]
+  enum priority: [ :low, :middle, :high ]
+  enum status: [ :pending, :processing, :completed ]
 end
